299afb57e422adb79a0d6e42b03d7dc74f917adf
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-05 18:52:29 -0400

    added 52 card deck

--- public/main.js
+++ public/main.js
@@ -1,11 +1,16 @@
 let deck = []
 
 const createDeck = () => {
-  let suits = ['Spade', 'Hearts', 'Diamonds', 'Clubs']
-
-  // let cardsBruno = [[2, 2], [3, 3], ['Ace', 11]]
-  let cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
-  let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+  // 1. create an array with suits
+  let suits = ['Spade', 'Heart', 'Diamond', 'Club']
+  // 2. create with cards with values
+  let cards = [[2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['Jack', 10], ['Queen', 10], ['King', 10], ['Ace', 11]]
+  // 4. create the deck
+  suits.forEach(suit => {
+    cards.forEach(card => {
+      deck.push(card.concat(suit))
+    })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', createDeck)
