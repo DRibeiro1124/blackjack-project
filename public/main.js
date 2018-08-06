052019950557c71049c8061e716867643ae1847b
Dino Ribeiro <DRibeiro1124@gmail.com>
2018-08-06 18:30:11 -0400

    player scores are now adding up

--- public/main.js
+++ public/main.js
@@ -1,11 +1,13 @@
 let deck = []
 let houseHand = []
 let playerHand = []
+let playerScore = 0
+let dealerScore = 0
 
 const createDeck = () => {
   // 1. create an array with suits
-  let suits = ['Spade', 'Heart', 'Diamond', 'Club']
-  // 2. create with cards with values
+  let suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs']
+  // 2. create cards with values (array inside and array)
   let cards = [[2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9], [10, 10], ['Jack', 10], ['Queen', 10], ['King', 10], ['Ace', 11]]
   // 4. create the deck
   suits.forEach(suit => {
@@ -45,21 +47,27 @@ const dealButtonClickEvent = () => {
   playerHand.push(deck.pop())
 
   // show the hand to the player
-  document.querySelector('#player-card1').innerHTML = playerHand[0]
-  document.querySelector('#player-card2').innerHTML = playerHand[1]
+  document.querySelector('#player-card1').textContent = playerHand[0][0] + ' of ' + playerHand[0][2]
+  document.querySelector('#player-card2').textContent = playerHand[1][0] + ' of ' + playerHand[1][2]
+  document.querySelector('#house-card1').textContent = houseHand[0][0] + ' of ' + houseHand[0][2]
+  playerScore += playerHand[0][1] + playerHand[1][1]
+  dealerScore += houseHand[0][1]
+  console.log(playerScore)
+  // add the value of player hand
 }
 
 const hitButtonClickEvent = () => {
   // player can now hit and draw another card or stand
-  // if the player hits, draw another card that it's displayed
+  // if the player hits, draw another card
   let drawnCard = deck.pop()
   const newLi = document.createElement('li')
-  newLi.textContent = drawnCard
+  // console.log(drawnCard)
+  newLi.textContent = drawnCard[0] + ' of ' + drawnCard[2]
   document.querySelector('#player-hand').appendChild(newLi)
-  console.log(drawnCard)
   playerHand.push(drawnCard)
 }
-
+// add the value for the cards dealt
+document.querySelector('.player-results')
 document.querySelector('.hit-button').addEventListener('click', hitButtonClickEvent)
 document.querySelector('#deal-button').addEventListener('click', dealButtonClickEvent)
 document.addEventListener('DOMContentLoaded', createDeck)
